3b29c073874a0145fb9e5b5d7f3c3effc6183ac4
Majeed Sofi <majeed.sofi@jungleworks.com>
2022-11-19 20:54:05 +0530

    updated packages

--- client/src/Layout/Home/Home.jsx
+++ client/src/Layout/Home/Home.jsx
@@ -4,6 +4,10 @@ import axios from "axios";
 import { PropagateLoader } from 'react-spinners';
 
 class Home extends Component {
+    render(){
+
+        return<h1></h1>
+    }
 }
 
 export default Home;
